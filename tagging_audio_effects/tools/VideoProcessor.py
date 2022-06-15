79f7f412eb086fcc97e7ab5b6f2eb842648345ce
Sabyasachi Ghosal <saby.ghosal@gmail.com>
2022-06-15 17:43:54 +0530

    Audio Processor for Videos

--- tagging_audio_effects/tools/VideoProcessor.py
+++ tagging_audio_effects/tools/VideoProcessor.py
@@ -1,41 +1,45 @@
+#!/usr/bin/python
 import subprocess
 import os
 import sys
-import glob
-from concurrent.futures import ProcessPoolExecutor as Executor
-from scipy.io import wavfile
+
+INPUT_VIDEO_PATH = sys.argv[1]
+OUTPUT_AUDIO_PATH = sys.argv[2]
+
+
+# INPUT_VIDEO_PATH = "/mnt/rds/redhen/gallina/tv/2022"
+# OUTPUT_AUDIO_PATH = "/home/sxg1263/sxg1263gallinahome/audio_output_files"
+# python VideoProcessor.py "/mnt/rds/redhen/gallina/tv/2022" "/home/sxg1263/sxg1263gallinahome/audio_output_files"
 
 class VideoProcessor:
     def __init__(self):
         self.cmd = "ffmpeg"
 
-    def extract_audio(self, video_file, output_ext="mp3"):
-        try:
-            filename, ext = os.path.splitext(video_file)
-            subprocess.call([self.cmd, "-i", video_file, "-vn", "-f", "wav", "-ar","16000",
-                             "-ac", "1", f"{filename}.{output_ext}"],
-                            stdout=subprocess.DEVNULL,
-                            stderr=subprocess.STDOUT)
-        except:
-            print("Error in converting ", video_file)
-            raise
+    def extract_audio(self, video_file, input_video_path, output_audio_file):
+        print("Video File " + video_file)
+        print("Input Video File " + input_video_path)
+        print("Output Video File " + output_audio_file)
+        subprocess.call([self.cmd, "-i", input_video_path, "-vn", "-f", "wav", "-ar", "16000",
+                         "-ac", "1", output_audio_file],
+                        stdout=subprocess.DEVNULL,
+                        stderr=subprocess.STDOUT)
 
-    def extract_audio_files(self, video_input_folder_path, audio_output_folder_path):
-        mp4_filenames = map(lambda x: x.split('.')[0], glob.iglob('*.mp4'))
-        POOL_SIZE = os.cpu_count()  # number of cores
-        with Executor(max_workers=POOL_SIZE) as executor:
-            executor.map(self.extract_audio, mp4_filenames)
+    def extract_audio_files(self, video_input_folder_path, audio_output_folder_path,
+                            video_file_extensions=".mp4", output_ext=".mp3"):
+        for dirpath, dirnames, filenames in os.walk(video_input_folder_path):
+            output_structure = os.path.join(audio_output_folder_path, dirpath[len(video_input_folder_path):])
+            input_structure = os.path.join(video_input_folder_path, dirpath[len(video_input_folder_path):])
+            if not os.path.isdir(output_structure):
+                os.mkdir(output_structure)
+            for filename in [f for f in filenames if f.endswith(video_file_extensions)]:
+                output_file_name = os.path.join(output_structure, filename)
+                output_file_name = os.path.splitext(output_file_name)[0] + output_ext
+                input_file_name = os.path.join(input_structure, filename)
+                self.extract_audio(filename, input_file_name, output_file_name)
+
 
 if __name__ == '__main__':
     vp = VideoProcessor()
-    print("Starting generation of audio files ....")
-    INPUT_VIDEO_PATH = "/Users/saby/Documents/RedHen/SampleCode/sample_redhen_files/"
-    OUTPUT_AUDIO_PATH = "/Users/saby/Documents/RedHen/SampleCode/output_files/"
-    #vp.extract_audio_files(INPUT_VIDEO_PATH, OUTPUT_AUDIO_PATH)"
-    vp.extract_audio(INPUT_VIDEO_PATH + "abc.mp4",output_ext="wav")
+    print("Starting generation of audio files from ", INPUT_VIDEO_PATH, " to ", OUTPUT_AUDIO_PATH)
+    vp.extract_audio_files(INPUT_VIDEO_PATH, OUTPUT_AUDIO_PATH)
     print("Generation of audio files complete...")
-    ##Analyse the output audio
-    sample_rate, wav_data = wavfile.read(INPUT_VIDEO_PATH + "abc.wav", 'rb')
-    print(f'Sample rate: {sample_rate} Hz')
-    print(f'Size of the input: {len(wav_data)}')
-
