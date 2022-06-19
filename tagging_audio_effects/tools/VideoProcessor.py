3d2a248005289eed874f85cbc8e3fce87dee9b64
Sabyasachi Ghosal <saby.ghosal@gmail.com>
2022-06-19 18:54:36 +0530

    Modifying the tool to generate audio files

--- tagging_audio_effects/tools/VideoProcessor.py
+++ tagging_audio_effects/tools/VideoProcessor.py
@@ -3,47 +3,50 @@ import subprocess
 import os
 import sys
 
-
-# This file is responsible for extracting the audio files from the Redhen Video Files
-# This file can be run using the command - python VideoProcessor.py <<source_folder>> <<output_folder>>
-
 INPUT_VIDEO_PATH = sys.argv[1]
 OUTPUT_AUDIO_PATH = sys.argv[2]
+OUTPUT_AUDIO_FORMAT = sys.argv[3]
+INPUT_VIDEO_FORMAT = sys.argv[4]
+LOGS = sys.argv[5]
 
-
-# INPUT_VIDEO_PATH = "/mnt/rds/redhen/gallina/tv/2022"
-# OUTPUT_AUDIO_PATH = "/home/sxg1263/sxg1263gallinahome/audio_output_files"
+# INPUT_VIDEO_PATH = "sample_redhen_files/" #"/mnt/rds/redhen/gallina/tv/2022"
+# OUTPUT_AUDIO_PATH = "output_files/" #"/home/sxg1263/sxg1263gallinahome/audio_output_files"
+# OUTPUT_AUDIO_FORMAT = "wav"
+# INPUT_VIDEO_FORMAT = "mp4"
+# LOGS = 0
 # python VideoProcessor.py "/mnt/rds/redhen/gallina/tv/2022" "/home/sxg1263/sxg1263gallinahome/audio_output_files"
 
 class VideoProcessor:
     def __init__(self):
         self.cmd = "ffmpeg"
 
-    def extract_audio(self, video_file, input_video_path, output_audio_file):
-        print("Video File " + video_file)
-        print("Input Video File " + input_video_path)
-        print("Output Video File " + output_audio_file)
-        subprocess.call([self.cmd, "-i", input_video_path, "-vn", "-f", "wav", "-ar", "16000",
+    def extract_audio(self, video_file, input_video_path, output_audio_file, output_ext, logs):
+        subprocess.call([self.cmd, "-i", input_video_path, "-vn", "-f", output_ext, "-ar", "16000",
                          "-ac", "1", output_audio_file],
                         stdout=subprocess.DEVNULL,
                         stderr=subprocess.STDOUT)
+        if logs:
+            print("Video File " + video_file)
+            print("Input Video File " + input_video_path)
+            print("Output Video File " + output_audio_file)
+            print("Generation of audio files complete for ", video_file)
 
     def extract_audio_files(self, video_input_folder_path, audio_output_folder_path,
-                            video_file_extensions=".mp4", output_ext=".mp3"):
+                            video_file_extensions="mp4", output_ext="wav", logs=0):
         for dirpath, dirnames, filenames in os.walk(video_input_folder_path):
             output_structure = os.path.join(audio_output_folder_path, dirpath[len(video_input_folder_path):])
             input_structure = os.path.join(video_input_folder_path, dirpath[len(video_input_folder_path):])
             if not os.path.isdir(output_structure):
                 os.mkdir(output_structure)
-            for filename in [f for f in filenames if f.endswith(video_file_extensions)]:
+            for filename in [f for f in filenames if f.endswith("." + video_file_extensions)]:
                 output_file_name = os.path.join(output_structure, filename)
-                output_file_name = os.path.splitext(output_file_name)[0] + output_ext
+                output_file_name = os.path.splitext(output_file_name)[0] + "." + output_ext
                 input_file_name = os.path.join(input_structure, filename)
-                self.extract_audio(filename, input_file_name, output_file_name)
-
+                self.extract_audio(filename, input_file_name, output_file_name, output_ext, logs)
 
 if __name__ == '__main__':
     vp = VideoProcessor()
-    print("Starting generation of audio files from ", INPUT_VIDEO_PATH, " to ", OUTPUT_AUDIO_PATH)
-    vp.extract_audio_files(INPUT_VIDEO_PATH, OUTPUT_AUDIO_PATH)
-    print("Generation of audio files complete...")
+    print("Starting generation of audio files from ", INPUT_VIDEO_PATH, " to ", OUTPUT_AUDIO_PATH
+          + " with format " + OUTPUT_AUDIO_FORMAT)
+    vp.extract_audio_files(INPUT_VIDEO_PATH, OUTPUT_AUDIO_PATH, INPUT_VIDEO_FORMAT, OUTPUT_AUDIO_FORMAT, LOGS)
+
