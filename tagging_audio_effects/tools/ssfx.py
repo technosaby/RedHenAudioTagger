f6c814a804713302db54d439fc926347da2b1330
Sabyasachi Ghosal <saby.ghosal@gmail.com>
2022-07-07 09:43:37 +0530

    Parser Script for metadata

--- tagging_audio_effects/tools/ssfx.py
+++ tagging_audio_effects/tools/ssfx.py
@@ -32,14 +32,32 @@ def get_time_interval_tags(scores_dict, tag_combinations, time_windows_indices, 
     return df
 
 
-def get_sfx_files(folder_path, date,  sfx_files_with_path, dir_names_date):
+def get_sfx_files(folder_path, date,  sfx_files_with_path):
     sfx_format = "sfx"
     for dir_path, dir_names, file_names in os.walk(folder_path):
         for file_name in [f for f in file_names if f.endswith("." + sfx_format) and f.startswith(str(date).split()[0])]:
             sfx_files_with_path.append(os.path.join(dir_path, file_name))
-            dir_names_date.append(dir_path.replace(folder_path, ''))
-    return sfx_files_with_path, dir_names_date
+    return sfx_files_with_path
 
+def filter_sfx_file(sfx_file, audio_effects, time_duration):
+    dict_times_score = OrderedDict()
+    with open(sfx_file) as f:
+        lines = f.readlines()
+        for line in lines[starting_line:]:
+            split_line = line.split(line_split_separator)
+            dict_times_score[split_line[0].rstrip("|").split("|")[1].replace(file_name_text, '')] = split_line[
+                1].lstrip("|")
+
+    # Creating time windows
+    time_windows_indices = [(i, time_duration + i) for i in range(0, int(float(list(dict_times_score.keys())[-1])))]
+    # ToDo:// create tags
+    tag_combinations = []
+    tag_combinations.append("Narration")
+    tag_combinations.append("Child speech")  # [["Narration", "Child speech"], ["Narration", "kid speaking"]]
+
+    # For each window find the tags
+    df = get_time_interval_tags(dict_times_score, tag_combinations, time_windows_indices, "and")
+    return df
 # ssfx -r —files /tv/2022/2022-03/20220305 —effects{{drum|percussion}&{laughter}} —within 5
 
 if __name__ == '__main__':
@@ -51,8 +69,8 @@ if __name__ == '__main__':
     # LOGS = sys.argv[6]
 
     FILE_LOCATION = "/Users/saby/Documents/RedHen/Baselining/TaggedAudioFiles/"
-    START_DATE = "2011-01-01"
-    END_DATE = "2021-01-01"
+    START_DATE = "2010-01-01"
+    END_DATE = "2020-01-01"
     EFFECTS = ["Narration", "Child speech"]
     TIME_DURATION = 500
     LOGS = 1
@@ -63,34 +81,16 @@ if __name__ == '__main__':
 
     date_range = pd.date_range(start=START_DATE, end=END_DATE)
     sfx_files_with_path = []
-    dir_names_date = []
     for date in date_range:
-        sfx_files_with_path, dir_names_date = get_sfx_files(FILE_LOCATION, date, sfx_files_with_path, dir_names_date)  # Get SFX files
+        sfx_files_with_path = get_sfx_files(FILE_LOCATION, date, sfx_files_with_path)  # Get SFX files
 
     for index, sfx_file in enumerate(sfx_files_with_path):
-        pass
-    csv_file_name = "2010-01-01_2335_US_CSPAN2_World_War_II_TAGS.csv"
-    file_name_text = "201001012335"
-    print("Starting the filtering script ...")
-    score_data = []
-    times = []
-    dict_times_score = OrderedDict()
-    with open(FILE_LOCATION) as f:
-        lines = f.readlines()
-        for line in lines[starting_line:]:
-            split_line = line.split(line_split_separator)
-            dict_times_score[split_line[0].rstrip("|").split("|")[1].replace(file_name_text, '')] = split_line[
-                1].lstrip("|")
+        csv_file_name = os.path.split(sfx_files_with_path[0])[1].replace(".sfx", ".csv")
+        file_name = os.path.split(sfx_files_with_path[0])[1].replace(".sfx", "")
+        file_name_text = "-".join(file_name.split("_", 2)[:2]).replace("-", "")
+        if LOGS: print("Starting the filtering script for filename ", sfx_file)
+        df = filter_sfx_file(sfx_file, EFFECTS, TIME_DURATION)
+        # Dump Dataframe to CSV
+        df.to_csv(csv_file_name)
 
-    # Creating time windows
-    time_windows_indices = [(i, TIME_DURATION + i) for i in range(0, int(float(list(dict_times_score.keys())[-1])))]
-    # ToDo:// create tags
-    tag_combinations = []
-    tag_combinations.append("Narration")
-    tag_combinations.append("Child speech")  # [["Narration", "Child speech"], ["Narration", "kid speaking"]]
-    selected_tags = []
-    # For each window find the tags
-    df = get_time_interval_tags(dict_times_score, tag_combinations, time_windows_indices, "and")
-    # Dump to CSV
-    df.to_csv(csv_file_name)
-    print("All Done ...")
+    if LOGS: print("All Done ...")
