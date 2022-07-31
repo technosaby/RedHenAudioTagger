34b22acdbd23c8027f89309e2379e3db036abc44
Sabyasachi Ghosal <89327872+technosaby@users.noreply.github.com>
2022-07-31 16:15:34 +0530

    Update hpc_script.sh

--- hpc_script.sh
+++ hpc_script.sh
@@ -3,6 +3,7 @@
 VIDEO_FILES=/mnt/rds/redhen/gallina/tv/2022/2022-01/2022-01-01
 SCRATCH_USER=/scratch/users/$USER
 TOOLS_FOLDER=$SCRATCH_USER/gsoc2022/tagging_audio_effects/tools
+ROOT_FOLDER=$SCRATCH_USER/gsoc2022/tagging_audio_effects
 
 # Load Module
 module load singularity/3.8.1
@@ -25,14 +26,18 @@ mkdir Output/
 cd Output
 mkdir AudioFiles
 mkdir TaggedAudioFiles
-mkdir FilteredOutput
 
 #Testing for 1 month videos 
 #singularity exec /scratch/users/$USER/image.sif python3 /scratch/users/$USER/gsoc2022/tagging_audio_effects/tools/audio_file_convertor.py -i /mnt/rds/redhen/gallina/tv/2022/2022-01/2022-01-01/ -a "wav" -o /scratch/users/$USER/Output/AudioFiles/  -l 0 --verbose=True
 singularity exec --bind $SCRATCH_USER $SCRATCH_USER/image.sif python3 $TOOLS_FOLDER/audio_file_convertor.py -i $SCRATCH_USER/VideosFiles/ -a "wav" -o $SCRATCH_USER/Output/AudioFiles/  -l 1
+echo "All the video files are converted to Audio, now run the tagging"
+singularity exec --bind $SCRATCH_USER $SCRATCH_USER/image.sif python3 $ROOT_FOLDER/tag_audio_effects.py -i $SCRATCH_USER/Output/AudioFiles/ -o $SCRATCH_USER/Output/TaggedAudioFiles/
+echo "All the audio files are tagged with the effects, now you can run queries on them"
 
-#Remove all copied files
-#rm -rf /scratch/users/$USER/image.sif
+#Remove all Video and Audio files
+rm -rf $SCRATCH_USER/VideosFiles
+rm -rf $SCRATCH_USER/Output/AudioFiles
+echo "All audio and video files removed, only tagged files present"
 
 
 
