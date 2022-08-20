66dab36ac313a9a1550a213ffae1b82f7b57e18d
Sabyasachi Ghosal <saby.ghosal@gmail.com>
2022-08-20 15:07:52 +0530

    Script preparation

--- README.md
+++ README.md
@@ -6,13 +6,19 @@ of Red Hen’s data. A single stream of data can contain multiple sound effects,
 from a group of known sound effects like a Multi-label classification problem. YamNet is used a pretrained model in this project.
 The video files are converted into audio files. Then they are tagged by YamNet for the sound effects and dumped into 
 2 kinds of files:
-- SFX Files: These files are based on RedHen's standards where tags are mapped with every frame of audio data.
+- SFX Files: These files are based on RedHen's standards where tags are mapped with every frame of audio data. JQ queries can 
+be used to filter the SFX tags.
 - CSV Files: These files contain tags and their times for the frames. These CSV files are consumed by ELAN tool for annotations
 based on the tiers as sound effects.
 
 ## Usage
 ### Singularity Environment
 A script called [hpc_script.py](hpc_script.sh) contains all the steps for running in a singularity container.
+1. Please clone the repo in RedHen's HPC as a scratch user in the home of the scratch user.
+2. Create a folder name with videos required for tagging or use an existing folder from RedHen's mount point.
+3. Pass the created folder name as an argument while running the file
+ ```hpc_script.sh <<folder_name_with videos>>```
+4. After the script is run, an Output folder will be generated with the tagged audio files.
 
 ## Future work
 - Evaluate other audio taggers and evaluate their results 
--- hpc_script.sh
+++ hpc_script.sh
@@ -1,45 +1,40 @@
 #!/bin/bash
 
-VIDEO_FILES=/mnt/rds/redhen/gallina/tv/2022/2022-01/2022-01-01
+VIDEO_FILES=$1 #/mnt/rds/redhen/gallina/tv/2022/2022-01/2022-01-01
 SCRATCH_USER=/scratch/users/$USER
 TOOLS_FOLDER=$SCRATCH_USER/gsoc2022/tagging_audio_effects/tools
 ROOT_FOLDER=$SCRATCH_USER/gsoc2022/tagging_audio_effects
-HOME_FOLDER=/home/$USER
+#HOME_FOLDER=/home/$USER
 
 # Load Module
 module load singularity/3.8.1
 
 # Change directory into $USER
-cd $SCRATCH_USER/
-
-# Copy one month video file from mnt to scratch
-mkdir VideosFiles
-cp -r $VIDEO_FILES $SCRATCH_USER/VideosFiles/
+cd $SCRATCH_USER/ || exit
 
 # Clone Source Code
 #git clone https://github.com/technosaby/gsoc2022.git
 
 # Creating Singularity Image from Github
-#singularity pull image.sif docker://ghcr.io/technosaby/gsoc2022-redhen-audio-tagging-stages:1
+singularity pull image.sif docker://ghcr.io/technosaby/gsoc2022-redhen-audio-tagging-stages:1
 
 # Creating Directories for Outputs
 mkdir Output/
-cd Output
+cd Output || exit
 mkdir AudioFiles
 mkdir TaggedAudioFiles
 
 #Testing for 1 month videos 
 #singularity exec /scratch/users/$USER/image.sif python3 /scratch/users/$USER/gsoc2022/tagging_audio_effects/tools/audio_file_convertor.py -i /mnt/rds/redhen/gallina/tv/2022/2022-01/2022-01-01/ -a "wav" -o /scratch/users/$USER/Output/AudioFiles/  -l 0 --verbose=True
-singularity exec --bind $SCRATCH_USER $SCRATCH_USER/image.sif python3 $TOOLS_FOLDER/audio_file_convertor.py -i $SCRATCH_USER/VideosFiles/ -a "wav" -o $SCRATCH_USER/Output/AudioFiles/  -l 1
+singularity exec --bind $SCRATCH_USER $SCRATCH_USER/image.sif python3 $TOOLS_FOLDER/audio_file_convertor.py -i $VIDEO_FILES -a "wav" -o $SCRATCH_USER/Output/AudioFiles/  -l 1
 echo "All the video files are converted to Audio, now run the tagging"
-singularity exec --bind $SCRATCH_USER $SCRATCH_USER/image.sif python3 $ROOT_FOLDER/tag_audio_effects.py -i $SCRATCH_USER/Output/AudioFiles/ -o $SCRATCH_USER/Output/TaggedAudioFiles/ -l 1
+singularity exec --bind $SCRATCH_USER $SCRATCH_USER/image.sif python3 $ROOT_FOLDER/tag_audio_effects.py -i $SCRATCH_USER/Output/AudioFiles/ -o $SCRATCH_USER/Output/TaggedAudioFiles/ -s 0.2 -l 1
 echo "All the audio files are tagged with the effects, now you can run queries on them"
 
 ## To filter tags
 #singularity exec --bind $SCRATCH_USER $SCRATCH_USER/image.sif python3 $TOOLS_FOLDER/ssfx.py -i 2022-01-01/ -q "(.Music // .song //.background), (.Television // .Tv)" -l 1
 
 #Remove all Video and Audio files
-rm -rf $SCRATCH_USER/VideosFiles
 rm -rf $SCRATCH_USER/Output/AudioFiles
 echo "All audio and video files removed, only tagged files present"
 
